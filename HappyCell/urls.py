7e8b9ccbab5db1d4f8d8ec0f51735ccfed4d0f32
Sandro Córdova <33694187+sandrocordova@users.noreply.github.com>
2022-07-18 16:19:39 -0500

    Modificar URL /menu

--- HappyCell/urls.py
+++ HappyCell/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from HappyCell.view import menu
+from view import menu
 
 urlpatterns = [
     path('admin/', admin.site.urls),
