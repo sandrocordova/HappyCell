4686f958a383452e04e63fde08c6c070a6855fe6
Sandro Córdova <33694187+sandrocordova@users.noreply.github.com>
2022-07-18 16:58:48 -0500

    Update urls.py

--- HappyCell/urls.py
+++ HappyCell/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from HappyCell.view import menu
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('menu/',include(('apps.transaccion.urls', 'app_name'), namespace='transaccion')),
+    path('', menu, name="menu"),
 ]
