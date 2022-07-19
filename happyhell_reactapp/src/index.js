d475a032e24a8bb0f607b4dcfdc09ff155dcb831
Carlos Andres Estrada Vasquez <49221016+caestrada4@users.noreply.github.com>
2022-07-19 12:53:30 -0500

    prueba

--- happyhell_reactapp/src/index.js
+++ happyhell_reactapp/src/index.js
@@ -7,7 +7,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <form>
-
+                <h2>
+                    SISTEMA DE ADMINISTRACION DE CARTERA
+                </h2>
+                <h2>
+                    Ciudad:Quito
+                    AGENCIA VILLAFLORA-COSTOS CAJA
+                </h2>
                 <label>Cedula:
                 <input type="Cedula" />
                 </label>       
