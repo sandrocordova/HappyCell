f068e2c377fb1b78f73ad37b06e29b9559b94b15
Carlos Andres Estrada Vasquez <49221016+caestrada4@users.noreply.github.com>
2022-07-19 08:09:33 -0500

    Cedula

--- happyhell_reactapp/src/App.js
+++ happyhell_reactapp/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
     return (
         <div className="container">
-            <h1>HappyCell</h1>
-            <p>I am writing JSX</p>
+            <h1>HappyCell</h1><h1>SISTEMA DE ADMINISTRACION DE CARTERA</h1><h1>Ciudad: Quito</h1>
+            <h1>Agencia</h1>
+
         </div>
     )
 }
--- happyhell_reactapp/src/index.js
+++ happyhell_reactapp/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'; // importamos react
 import ReactDOM from 'react-dom'; // nos permite renderizar en el DOM
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function VistaInicial(){
+        return (
+            <form>
+                <label>Cedula:
+                    <input type="Cedula" />
+                    <input type="Nombre" />
+                </label>
+
+            </form>
+            
+            )
+}
 
 
 //selecionamos el id donde se renderizara el componente
 const root = document.getElementById('root'); // hay un div con id root en index.html
+root.render(<VistaInicial />)
