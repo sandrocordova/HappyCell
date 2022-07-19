66fcb66ead7fb7b32791a077b49403d130801577
Carlos Andres Estrada Vasquez <49221016+caestrada4@users.noreply.github.com>
2022-07-19 11:01:06 -0500

    Update2

--- happyhell_reactapp/src/index.js
+++ happyhell_reactapp/src/index.js
@@ -1,20 +1,21 @@
 import React from 'react'; // importamos react
-//import ReactDOM from 'react-dom'; // nos permite renderizar en el DOM
+import ReactDOM from 'react-dom'; // nos permite renderizar en el DOM
 import './index.css';
 import App from './App';
 const root = document.getElementById('root'); // hay un div con id root en index.html
-function VistaInicial(){
-        return (
-            <form>
-                <label>Cedula:
-                    <input type="Cedula" />
-                    <input type="Nombre" />
-                </label>
-                <App />
-            </form>
-            
-            )
-}
+
+root.render(
+    <React.StrictMode>
+        <form>
+            <label>Cedula:
+                <input type="Cedula" />
+                <input type="Nombre" />
+            </label>
+            <App />
+        </form>
+    </React.StrictMode>
+);
+
 
 
 //selecionamos el id donde se renderizara el componente
