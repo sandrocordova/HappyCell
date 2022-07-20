94228e8a0643709f36d255a2a1592ff4e13e598d
Carlos Andres Estrada Vasquez <49221016+caestrada4@users.noreply.github.com>
2022-07-20 14:05:53 -0500

    Creacion del menu

--- happyhell_reactapp/src/App.js
+++ happyhell_reactapp/src/App.js
@@ -1,41 +1,15 @@
+import react from 'react'
 import './App.css';
+import Navbar from './components/navbar-menu';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
     return (
-        <>
-            <div class="sizeLetra">
-                <div className="table-responsive">
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Nro. Credito</th>
-                                <th>Fecha Concesion</th>
-                                <th>Monto Financiado</th>
-                                <th>Saldo Adeudado</th>
-                                <th>Estado</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1234-00</td>
-                                <td>31-12-2021</td>
-                                <td>$400</td>
-                                <td>$100</td>
-                                <td>DESEMBOLSADO</td>
-                            </tr>
-                            <tr>
-                                <td>1567-00</td>
-                                <td>31-12-2021</td>
-                                <td>$300</td>
-                                <td>$100</td>
-                                <td>DESEMBOLSADO</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </>
+        <Router>
+            <Navbar />
 
+        </Router>
     );
 }
 
