dcdcefb45bf569756cf485139eec8f16ed3106fa
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-07-20 11:02:59 -0500

    Corrección de Modelos

--- apps/transaccion/views.py
+++ apps/transaccion/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from apps.transaccion.models import Agencia
+from apps.transaccion.models import AGENCIA
+
 
 # Create your views here.
 
 
 def menu(request):
-    agencia = Agencia.objects.all()
+    agencia = AGENCIA.objects.all()
     context = {'agencias': agencia}
+    print(agencia+"--------------------------------------------------------------")
     return render(request, 'menu.html', context)
