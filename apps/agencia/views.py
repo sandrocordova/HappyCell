c8a65a0e7aa7e49535ff12e2dc748682fb6658e0
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-07-20 15:09:30 -0500

    buscar error

--- apps/agencia/views.py
+++ apps/agencia/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from apps.agencia.models import Agencia
@@ -10,13 +11,15 @@ def menu(request):
     #AGENCIA = Agencia.objects.raw("SELECT * FROM AGENCIA")
     #AGENCIA = Agencia.objects.raw("SELECT AGEN_CODIGO, AGEN_DESCRIPCION, AGEN_DIRECCION, AGEN_RESPONSABLE, AGEN_TELEFONO, AGEN_CODIGO_SUPER, CIUD_CODIGO FROM [SEGURIDAD_APP].[dbo].[AGENCIA]")[0]
     #AGENCIA = Agencia.objects.raw("SELECT 1 as id AGEN_CODIGO, AGEN_DESCRIPCION, AGEN_DIRECCION, AGEN_RESPONSABLE, AGEN_TELEFONO, AGEN_CODIGO_SUPER, CIUD_CODIGO FROM [SEGURIDAD_APP].[dbo].[AGENCIA]")
-    AGENCIA = Agencia.objects.raw("SELECT AGEN_CODIGO, AGEN_DESCRIPCION, AGEN_DIRECCION, AGEN_RESPONSABLE, AGEN_TELEFONO, AGEN_CODIGO_SUPER, CIUD_CODIGO FROM [SEGURIDAD_APP].[dbo].[AGENCIA]")
+    entrada = Agencia.objects.raw("SELECT AGEN_CODIGO, AGEN_DESCRIPCION, AGEN_DIRECCION, AGEN_RESPONSABLE, AGEN_TELEFONO, AGEN_CODIGO_SUPER, CIUD_CODIGO FROM [SEGURIDAD_APP].[dbo].[AGENCIA]")
+    for s in Agencia.objects.raw("SELECT AGEN_CODIGO, AGEN_DESCRIPCION, AGEN_DIRECCION, AGEN_RESPONSABLE, AGEN_TELEFONO, AGEN_CODIGO_SUPER, CIUD_CODIGO FROM AGENCIA"):
+        print(s)
     
-    context = {'agencias': AGENCIA}
+    context = {'agencias': entrada}
     print("HIZO LA CONSULTA ----------------------------")
-    print(AGENCIA)
+    print(entrada)
     print(context)
-    print(Agencia)
+    print(connection.queries)
     
     print("HIZO LA CONSULTA ---------------------------- FIN")
     return render(request, 'menu.html', context)
