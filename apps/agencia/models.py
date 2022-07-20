1c0097743f3bd09cbb0215bafbcd798834333f67
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-07-20 13:53:04 -0500

    Eliminar FKs

--- apps/agencia/models.py
+++ apps/agencia/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 
 class Agencia(models.Model):
-    zona_codigo = models.ForeignKey('Zona', models.DO_NOTHING, db_column='ZONA_CODIGO', primary_key=True)  # Field name made lowercase.
-    empr_codigo = models.ForeignKey('Empresa', models.DO_NOTHING, db_column='EMPR_CODIGO')  # Field name made lowercase.
     agen_codigo = models.DecimalField(db_column='AGEN_CODIGO', max_digits=4, decimal_places=0)  # Field name made lowercase.
-    tiag_codigo = models.ForeignKey('TipoAgencia', models.DO_NOTHING, db_column='TIAG_CODIGO')  # Field name made lowercase.
     agen_descripcion = models.CharField(db_column='AGEN_DESCRIPCION', max_length=40)  # Field name made lowercase.
     agen_direccion = models.CharField(db_column='AGEN_DIRECCION', max_length=80)  # Field name made lowercase.
     agen_responsable = models.CharField(db_column='AGEN_RESPONSABLE', max_length=40)  # Field name made lowercase.
