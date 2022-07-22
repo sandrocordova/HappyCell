55261d6dd2bc247207510608375f55e052625982
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-07-22 10:39:56 -0500

    menores

--- apps/agencia/api/urls.py
+++ apps/agencia/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.agencia.api.views import PostApiViewSet
+from apps.agencia.api.views import PostApiViewSet, nav_api_view
 
 
 urlpatterns = [
-    path('empresa', PostApiViewSet.as_view(), name='empresa_api'),
+    #path('empresa', PostApiViewSetClass.as_view(), name='empresa_api'),
+    path('info', nav_api_view, name='api_nav'),
+    path('empresa/<int:id>', PostApiViewSet.as_view(), name='empresa_api_id'),
 ]
--- apps/agencia/api/views.py
+++ apps/agencia/api/views.py
@@ -3,9 +3,47 @@ from rest_framework.response import Response
 from apps.agencia.models import Empresa
 from apps.agencia.api.serializer import PostSerializer
 
+@api_view(['GET'])
+def nav_api_view(request):
+    
+    if request.method == 'GET':
+        consulta = Empresa.objects.raw("SELECT EMPR_CODIGO, EMPR_NOMBRE, EMPR_IDENTIFICACION FROM [SEGURIDAD_APP].[dbo].[EMPRESA]")  
+        serializer_empresas = PostSerializer(consulta, many = True)
+        return Response(serializer_empresas.data)
+    
+@api_view(['GET'])
+def nav_infor_api_view(request):
+    
+    if request.method == 'GET':
+        consulta = Empresa.objects.raw("SELECT EMPR_CODIGO, EMPR_NOMBRE, EMPR_IDENTIFICACION FROM [SEGURIDAD_APP].[dbo].[EMPRESA]")  
+        serializer_empresas = PostSerializer(consulta, many = True)
+        return Response(serializer_empresas.data)
+    
+@api_view(['GET'])
+def detail_view_set(request):
+    
+    if request.method == 'GET':
+        consulta = Empresa.objects.raw("SELECT EMPR_CODIGO, EMPR_NOMBRE, EMPR_IDENTIFICACION FROM [SEGURIDAD_APP].[dbo].[EMPRESA]")  
+        serializer_empresas = PostSerializer(consulta, many = True)
+        return Response(serializer_empresas.data)
+    
+@api_view(['GET','POST'])
+def PostApiViewSet_Post(request):
+    
+    if request.method == 'GET':
+        consulta = Empresa.objects.raw("SELECT EMPR_CODIGO, EMPR_NOMBRE, EMPR_IDENTIFICACION FROM [SEGURIDAD_APP].[dbo].[EMPRESA]")  
+        serializer_empresas = PostSerializer(consulta, many = True)
+        return Response(serializer_empresas.data)
+    
+    elif request.method == 'POST':
+        serializer_empresas = PostSerializer(data = request.data)
+        if serializer_empresas.isValid():
+            serializer_empresas.save()
+            return Response(serializer_empresas.data)
+        return Response(serializer_empresas.errors)
 
-# Create your views here.
-class PostApiViewSet(APIView):
+# Basado en clases.
+class PostApiViewSetClass(APIView):
     
     def get(self, request):
         empresas = Empresa.objects.raw("SELECT EMPR_CODIGO, EMPR_NOMBRE, EMPR_IDENTIFICACION FROM [SEGURIDAD_APP].[dbo].[EMPRESA]")  
