8f098aeac1c1ae605b67ccac1b79fddfa3bc31a4
Carlos Andres Estrada Vasquez <49221016+caestrada4@users.noreply.github.com>
2022-07-26 13:29:31 -0500

    Navbar Funcionando

--- happyhell_reactapp/src/App.js
+++ happyhell_reactapp/src/App.js
@@ -1,37 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Navbar from './components/navbar-menu';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Admin from './pages/admin';
-import Documents from './pages/documents';
-import Fabrica from './pages/fabrica';
-import Massive from './pages/massive';
-import Messages from './pages/messages';
-import Paymen from './pages/paymen';
-import Polit from './pages/polit';
-import Process from './pages/process';
-import Support from './pages/support';
-import Consultas from './pages/consultas';
-
-function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/Administracion' component={Admin} />
-                <Route path='/Politicas' component={Polit} />
-                <Route path='/Pagos' component={Paymen} />
-                <Route path='/ProcesosECRE' component={Process} />
-                <Route path='/DocumentosC' component={Documents} />
-                <Route path='/FabricaDCRE' component={Fabrica} />
-                <Route path='/MotorMensajeria' component={Messages} />
-                <Route path='/Consultas' component={Consultas} />
-                <Route path='/ProcesosMA' component={Massive} />
-                <Route path='/Soporte' component={Support} />
-            </Routes>
-        </Router>
-=======
 import NavBarHeader from './components/nav-menu/nav-header.component';
 import Navbar from './components/navbar-menu';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -45,42 +13,13 @@ import Polit from './pages/polit';
 import Process from './pages/process';
 import Support from './pages/support';
 import Consultas from './pages/consultas';
+import Navopciones from './components/navopciones/navopciones'
 
 function App() {
+
     return (
         <>
-            <NavBarHeader></NavBarHeader>
-            {/*<div class="sizeLetra">
-                <div className="table-responsive">
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Nro. Credito</th>
-                                <th>Fecha Concesion</th>
-                                <th>Monto Financiado</th>
-                                <th>Saldo Adeudado</th>
-                                <th>Estado</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1234-00</td>
-                                <td>31-12-2021</td>
-                                <td>$400</td>
-                                <td>$100</td>
-                                <td>DESEMBOLSADO</td>
-                            </tr>
-                            <tr>
-                                <td>1567-00</td>
-                                <td>31-12-2021</td>
-                                <td>$300</td>
-                                <td>$100</td>
-                                <td>DESEMBOLSADO</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-    </div>*/}
+            <Navopciones></Navopciones>
             <Router>
                 <Navbar />
                 <Routes>
@@ -96,10 +35,8 @@ function App() {
                     <Route path='/Soporte' component={Support} />
                 </Routes>
             </Router>
-
         </>
 
->>>>>>> 95b371c66963429b8e1af53e66829053b321fa8c
     );
 }
 
--- happyhell_reactapp/src/index.js
+++ happyhell_reactapp/src/index.js
@@ -1,20 +1,11 @@
 import React from 'react'; // importamos react
-import ReactDOM from 'react-dom'; // nos permite renderizar en el DOM
+import ReactDOM from 'react-dom/client'; // nos permite renderizar en el DOM
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-            <App />
-<<<<<<< HEAD
-       
-=======
->>>>>>> 95b371c66963429b8e1af53e66829053b321fa8c
+        <App />
     </React.StrictMode>
 );
-
-
-
-//selecionamos el id donde se renderizara el componente
-
