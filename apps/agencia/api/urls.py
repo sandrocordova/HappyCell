89c54ab18a1edb8a43aff4b7777a43e753a91ac2
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-07-26 12:43:19 -0500

    menores

--- apps/agencia/api/urls.py
+++ apps/agencia/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     #path('empresa', PostApiViewSetClass.as_view(), name='empresa_api'),
     path('usernav', usernav_api_view, name='api_user_nav'),
     path('usernavdos', nav_infor_api_view, name='api_user_nav_dos'),
-    path('empresa', nav_infor_api_view, name='api_empresa'),
+    path('empresa', detail_view_set, name='api_empresa'),
     #path('empresa/<int:pk>', detail_view_set, name='empresa_api_id'),
 ]
