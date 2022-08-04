068ae40d1aadc45607860e1dd5f74ea73e178f82
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-08-04 10:40:04 -0500

    Creación de URLs mensajería

--- happyhell_reactapp/src/index.js
+++ happyhell_reactapp/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Clientespage from './pages/Clientes.page';
-import Mensajeriapage from './pages/Mensajeria.page';
 import Mantenimientopage from './pages/Mantenimiento.page';
 import Clientesvistapage from './pages/Clientesvista.page';
 
+import Mensajeriapage from './pages/Mensajeria.page';
+import MensajeriaParametrospage from './pages/Mensajeria.parametros.page';
+import MensajeriaPlantillapage from './pages/Mensajeria.plantilla.page';
+import MensajeriaEjecucionpage from './pages/Mensajeria.ejecucion.page';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,9 +23,14 @@ root.render(
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="clientes" element={<Clientespage />} />
-                <Route path="mensajeria" element={<Mensajeriapage />} />
                 <Route path="clientes/mantenimiento" element={<Mantenimientopage />} />
                 <Route path="clientes/vistaclientes" element={<Clientesvistapage />} />
+
+                <Route path="mensajeria" element={<Mensajeriapage />} />
+                <Route path="mensajeria/parametros" element={<MensajeriaParametrospage />} />
+                <Route path="mensajeria/plantillas" element={<MensajeriaPlantillapage />} />
+                <Route path="mensajeria/ejecucion" element={<MensajeriaEjecucionpage />} />
+                
 
 
             </Routes>
