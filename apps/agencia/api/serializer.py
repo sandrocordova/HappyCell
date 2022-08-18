9a2fa399bad9af82a6903eb8d3422537e5966735
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-08-18 14:42:58 -0500

    Menores

--- apps/agencia/api/serializer.py
+++ apps/agencia/api/serializer.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 from apps.agencia.models import Empresa, Usernav, Usernavtres
-from apps.agencia.models import Profesiones, Nacionalidad, ActiEconomica, TipoRol, Sexo, Vivienda, EstadoCivil, SituacionLaboral
-from apps.cliente.models import Cliente
 
 # Create your views here.
 class PostSerializer(serializers.ModelSerializer):
@@ -22,47 +20,3 @@ class UserNavSerializerDos(serializers.ModelSerializer):
     class Meta:
         model = Usernavtres
         fields = '__all__'
-        
-class ClienteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cliente
-        fields = '__all__'
-        
-        #Catalogos
-class ProfesionesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profesiones
-        fields = '__all__'
-
-class NacionalidadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Nacionalidad
-        fields = '__all__'
-
-class ActiEconomicaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ActiEconomica
-        fields = '__all__'
-
-class TipoRolSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TipoRol
-        fields = '__all__'
-
-class SexoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sexo
-        fields = '__all__'
-class ViviendaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vivienda
-        fields = '__all__'
-class EstadoCivilSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EstadoCivil
-        fields = '__all__'
-class SituacionLaboralCivilSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SituacionLaboral
-        fields = '__all__'
-        
--- apps/agencia/api/urls.py
+++ apps/agencia/api/urls.py
@@ -1,21 +1,8 @@
 from django.urls import path
-from apps.agencia.api.views import nav_infor_api_view, usernav_api_view, detail_view_set, cliente_api_view, profesiones_api_views
-from apps.agencia.api.views import profesiones_api_views, nacionalidad_api_views, acti_economica_api_views, tipo_rol_api_views, sexo_api_views, vivienda_api_views, estado_civil_api_views, situacion_laboral_api_views
-
+from apps.agencia.api.views import nav_infor_api_view, usernav_api_view, detail_view_set
 
 urlpatterns = [
-    #path('empresa', PostApiViewSetClass.as_view(), name='empresa_api'),
     path('usernav', usernav_api_view, name='api_user_nav'),
     path('usernavdos', nav_infor_api_view, name='api_user_nav_dos'),
     path('empresa', detail_view_set, name='api_empresa'),
-    path('cliente', cliente_api_view, name='api_cliente'),
-    path('cat/1', profesiones_api_views.as_view()),
-    path('cat/2', nacionalidad_api_views.as_view()),
-    path('cat/3', acti_economica_api_views.as_view()),
-    path('cat/4', tipo_rol_api_views.as_view()),
-    path('cat/5', sexo_api_views.as_view()),
-    path('cat/6', vivienda_api_views.as_view()),
-    path('cat/7', estado_civil_api_views.as_view()),
-    path('cat/8', situacion_laboral_api_views.as_view()),
-    #path('empresa/<int:pk>', detail_view_set, name='empresa_api_id'),
 ]
