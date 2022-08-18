dce84dd2e1b0aa47e8df3cdda5f9398c78f3572e
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-08-18 12:56:12 -0500

    menores

--- HappyCell/urls.py
+++ HappyCell/urls.py
@@ -1,17 +1,3 @@
-"""HappyCell URL Configuration
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from HappyCell.view import menu
@@ -21,7 +7,7 @@ urlpatterns = [
     path('api/', include('apps.agencia.api.urls')),
     path('agencia/', include(('apps.agencia.urls', 'app_name'), namespace='agencia')),
     path('cliente/', include(('apps.cliente.urls', 'app_name'), namespace='cliente')),
-    path('cat/', include(('apps.catalog.urls', 'app_name'), namespace='catalogo')),
+    path('cat/', include('apps.catalog.urls', 'app_name')),
     path('', menu, name="menu"),
     path('api-cli/v1/', include('apps.apihc.urls')),
 ]
