468bae94c643f9a66773672fa6b9d0ef3d9c4660
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-08-19 12:24:37 -0500

    Creación de API unificada

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -7,6 +7,45 @@ from apps.catalog.serializer import ProfesionesSerializer, ProfesionesSerializer
 #Catálogos
 class profesiones_api_views(APIView):
     def get(self, request):
+        catalog_id = ['profesion', 'nacionalidad', 'actividad_economica', 'tipo_rol', 'sexo', 'vivienda', 'estado_civil', 'situacion_laboral']
+        catalog_list =[]
+        if 'profesion' in catalog_id:
+            consulta = Profesiones.objects.using('clientes').all()
+            profesionesSerializer = ProfesionesSerializer(consulta, many = True)
+            catalog_list.append({'profesion':profesionesSerializer.data})
+        if 'nacionalidad' in catalog_id:
+            consulta = Nacionalidad.objects.using('clientes').all()
+            nacionalidadSerializer = NacionalidadSerializer(consulta, many = True)
+            catalog_list.append({'nacionalidad':nacionalidadSerializer.data})
+        if 'actividad_economica' in catalog_id:
+            consulta = ActiEconomica.objects.using('clientes').all()
+            actiEconomicaSerializer = ActiEconomicaSerializer(consulta, many = True)
+            catalog_list.append({'actividad_economica':actiEconomicaSerializer.data})
+        if 'tipo_rol' in catalog_id:
+            consulta = TipoRol.objects.using('clientes').all()
+            tipoRolSerializer = TipoRolSerializer(consulta, many = True)
+            catalog_list.append({'tipo_rol':tipoRolSerializer.data})
+        if 'sexo' in catalog_id:
+            consulta = Sexo.objects.using('clientes').all()
+            sexoSerializer = SexoSerializer(consulta, many = True)
+            catalog_list.append({'sexo':sexoSerializer.data})
+        if 'vivienda' in catalog_id:
+            consulta = Vivienda.objects.using('clientes').all()
+            viviendaSerializer = ViviendaSerializer(consulta, many = True)
+            catalog_list.append({'vivienda':viviendaSerializer.data})
+        if 'estado_civil' in catalog_id:
+            consulta = EstadoCivil.objects.using('clientes').all()
+            estadoCivilSerializer = EstadoCivilSerializer(consulta, many = True)
+            catalog_list.append({'estado_civil':estadoCivilSerializer.data})
+        if 'situacion_laboral' in catalog_id:
+            consulta = SituacionLaboral.objects.using('clientes').all()
+            situLaboralSerializer = SituacionLaboralCivilSerializer(consulta, many = True)
+            catalog_list.append({'situacion_laboral':situLaboralSerializer.data})
+        return Response(profesionesSerializer.data, status = status.HTTP_200_OK)
+    
+    
+class profesiones_api_views(APIView):
+    def get(self, request):
         consulta = Profesiones.objects.using('clientes').all()
         profesionesSerializer = ProfesionesSerializer(consulta, many = True)
         return Response(profesionesSerializer.data, status = status.HTTP_200_OK)
