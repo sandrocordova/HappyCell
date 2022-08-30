a94967262e796d326be9e4c60f49ce814f953975
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-08-30 17:11:31 -0500

    URL de user-data

--- HappyCell/urls.py
+++ HappyCell/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('agencia/', include(('apps.agencia.urls', 'app_name'), namespace='agencia')),
     path('cliente/', include(('apps.cliente.urls', 'app_name'), namespace='cliente')),
     path('cat/', include('apps.catalog.urls')),
-    path('api-user-data/v1', include('apps.agencia.api.urls')),
+    path('api-user-data/v1/', include('apps.agencia.api.urls')),
     path('api-cli/v1/', include('apps.apihc.urls')),
     path('api-dir/v1/', include('apps.direccion.urls')),
     path('api-obs/v1/', include('apps.observacion.urls')),
--- apps/agencia/api/urls.py
+++ apps/agencia/api/urls.py
@@ -3,6 +3,6 @@ from apps.agencia.api.views import nav_infor_api_view, usernav_api_view, detail_
 
 urlpatterns = [
     path('user-data', usernav_api_view, name='api_user_nav'),
-    path('usernavdos', nav_infor_api_view, name='api_user_nav_dos'),
+    path('user-nav-menu', nav_infor_api_view, name='api_user_nav_dos'),
     path('empresa', detail_view_set, name='api_empresa'),
 ]
