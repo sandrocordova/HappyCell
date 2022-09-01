5d32e2bf06a13982a97cd6b748869a1011474cb0
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-09-01 16:39:49 -0500

    Implementar funcionalidad de busqueda cliente

--- happycell_versionfinal/src/Api/apicall_search.js
+++ happycell_versionfinal/src/Api/apicall_search.js
@@ -1,9 +1,32 @@
 import { API } from '../config';
 
+// metodo para buscar clientes
 export const searchClient = async (text, page) => {
     const mySearch = { data: text }
     try {
         const res = await fetch(`${API}/cliente/search?page=${page}`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(mySearch)
+        });
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+}
+
+// metodo para obtener el detalle del cliente
+export const searchClientDetail = async (type, clieCodigo) => {
+    const mySearch = {
+        CLIE_CODIGO: clieCodigo,
+        TICL_CODIGO: type
+    }
+    try {
+        const res = await fetch(`${API}/cliente/detalle`, {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
--- happycell_versionfinal/src/redux/store.js
+++ happycell_versionfinal/src/redux/store.js
@@ -1,10 +1,52 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query/react';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+
 import themeOptionsReducer from '../features/themeOptions/themeOptions';
 import clientsReducer from '../features/clients/clientsSlice';
+import catalogsSlice from '../features/catalogs/catalogsSlice';
+import { apicall_catalogs } from '../Api/apicall_catalogs';
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+    blacklist: ['getCatalogs'],
+};
+
+const rootReducer = combineReducers({
+    themeOptions: themeOptionsReducer,
+    client: clientsReducer,
+    catalogs: catalogsSlice,
+    [apicall_catalogs.reducerPath]: apicall_catalogs.reducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        themeOptions: themeOptionsReducer,
-        client: clientsReducer
-    },
+    reducer: persistedReducer,
+    // Adding the api middleware enables caching, invalidation, polling,
+    // and other useful features of `rtk-query`.
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(apicall_catalogs.middleware),
 })
+
+export let persistor = persistStore(store)
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch)
