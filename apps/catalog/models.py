ffcde9cd6c14aed37eb642b2df5fefb5b853016f
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-09-05 15:04:27 -0500

    Menores

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -274,7 +274,7 @@ class TipoClase(models.Model):
 
 class TipoRol(models.Model):
     TIRO_CODIGO = models.CharField(max_length = 2, primary_key = True)
-    TIROL_DESCRIPCIÒN = models.CharField(max_length = 50)
+    TIROL_DESCRIPCION = models.CharField(max_length = 50)
 
     class Meta:
         db_table = 'TIPO_ROL'
