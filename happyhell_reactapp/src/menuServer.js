976abcd740b04763ba62920de0a74a9ca83f4ea2
Sandro Cordova <sandromichaelcordovacarrion@gmail.com>
2022-09-06 15:27:24 -0500

    Eliminación de Basura

--- happyhell_reactapp/src/menuServer.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-const API_URL = "http://192.168.88.103:8080/api/empresa"
-
-export const menu = async () => {
-    return await fetch(API_URL)
-}
--- happyhell_reactapp/src/models/User/menu-items.model.js
+++
@@ -1,8 +0,0 @@
-export class User {
-    constructor(label, value, path, main) {
-      this.label = label;
-      this.value = value;
-      this.path = path;
-      this.main = main;
-    }
-  }
